da37b1e837e2ee0fca52beb10e4adbfd292898ba
Maatt0s <juullia.mattos@gmail.com>
2025-09-06 11:42:14 -0300

    return

--- Models/Coin.cs
+++ Models/Coin.cs
@@ -24,6 +24,8 @@ namespace MauiApp1.Models
             string resultado = (Lado == ladoEscolhido) ?
                 $"Parabéns, você pediu {ladoEscolhido} e deu {Lado}" :
                 $"Que pena, você pediu {ladoEscolhido} e deu {Lado}";
+
+            return resultado;
         }
     }
 }
