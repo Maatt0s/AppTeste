556ff20cd9e576634ad0e1b15d2ee89a570542cc
Maatt0s <juullia.mattos@gmail.com>
2025-09-06 11:23:20 -0300

    .

---
+++ ViewModels/CoinViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace MauiApp1.ViewModels
+{
+    public partial class CoinViewModel : ObservableObject
+    {
+        public String _ladoEscolhido { get; set; } = String.Empty;
+        public String _imagem { get; set; } = String.Empty;
+        public String _resultado { get; set; } = String.Empty;
+
+
+    }
+}
